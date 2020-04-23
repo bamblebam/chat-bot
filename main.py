7b52120ef5989075fa0382b931127d306948d1ab
bamblebam <burhanizrangwala@gmail.com>
2020-04-23 21:39:58 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,106 @@
+import nltk
+from nltk.stem.lancaster import *
+import tensorflow
+import numpy
+import json
+import random
+import tflearn
+import pickle
+
+
+stemmer=LancasterStemmer()
+
+with open("intents.json") as file:
+    data=json.load(file)
+try:
+    with open("data.pickle","rb") as f:
+        words,labels,training,output=pickle.load(f)
+except:
+    words=[]
+    labels=[]
+    docs_x=[]
+    docs_y=[]
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            token_word=nltk.word_tokenize(pattern)
+            words.extend(token_word)
+            docs_x.append(token_word)
+            docs_y.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words=[stemmer.stem(word.lower()) for word in words if word != "?"]
+    words=sorted(list(set(words)))
+
+    labels=sorted(labels)
+
+    training=[]
+    output=[]
+    out_empty=[0 for _ in range(len(labels))]
+
+    for x,doc in enumerate(docs_x):
+        bag=[]
+        token_word=[stemmer.stem(word) for word in doc]
+        for word in words:
+            if word in token_word:
+                bag.append(1)
+            else:
+                bag.append(0)
+        output_row=out_empty[:]
+        output_row[labels.index(docs_y[x])]=1
+        training.append(bag)
+        output.append(output_row)
+
+    training=numpy.array(training)
+    output=numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words,labels,training,output),f)
+
+
+tensorflow.reset_default_graph()
+
+net=tflearn.input_data(shape=[None,len(training[0])])
+net=tflearn.fully_connected(net,8)
+net=tflearn.fully_connected(net,8)
+net=tflearn.fully_connected(net,len(output[0]),activation="softmax")
+net=tflearn.regression(net)
+
+model=tflearn.DNN(net)
+try:
+
+    model.load("model.tflearn")
+except:
+    model.fit(training,output,n_epoch=1000,batch_size=8,show_metric=True)
+    model.save("model.tflearn")
+
+def bag_of_words(s,words):
+    bag=[0 for _ in range(len(words))]
+    s_words=nltk.word_tokenize(s)
+    s_words=[stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i,w in enumerate(words):
+            if w==se:
+                bag[i]=1
+    return numpy.array(bag)
+
+def chat():
+    print("chat with bot")
+    while(True):
+        inp=input("You: ")
+        if inp.lower()=="quit":
+            break
+        results=model.predict([bag_of_words(inp,words)])[0]
+        results_index=numpy.argmax(results)
+        tag=labels[results_index]
+        if results[results_index]>0.7:
+            for tg in data["intents"]:
+                if tg["tag"]==tag:
+                     response=tg["responses"]
+            print(random.choice(response))
+        else:
+            print("I didnt get that :(")
+
+chat()
